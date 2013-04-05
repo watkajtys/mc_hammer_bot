ce4d17e8f778edea6907e3d2fcb869026643ca0d
Matt Watkajtys <watkajtys@gmail.com>
2013-04-05 17:09:32 -0400

    Added fine motor control mode

--- bot_control/app/assets/javascripts/botcontrol.js
+++ bot_control/app/assets/javascripts/botcontrol.js
@@ -5,28 +5,29 @@ $(function(){
 
    var testing = "http://127.0.0.1:8071"
    var real = "http://192.168.1.54:8071"
+   var speed = 1
 
    function forward(){
       console.log('moving');
-      $.ajax({url: testing+"/motion-control/update", data:{forward: +1}, dataType: "jsonp"});
+      $.ajax({url: testing+"/motion-control/update", data:{forward: +speed}, dataType: "jsonp"});
       status.html("Moving Forward");
    };
 
    function backward(){
       console.log('backwards');
-      $.ajax({url: testing+"/motion-control/update", data:{forward: -1}, dataType: "jsonp"});
+      $.ajax({url: testing+"/motion-control/update", data:{forward: -speed}, dataType: "jsonp"});
       status.html("Moving Backwards");
    };
 
    function right(){
       console.log('turn right');
-      $.ajax({url: testing+'/motion-control/update', data:{turn: +1}, dataType: "jsonp"});
+      $.ajax({url: testing+'/motion-control/update', data:{turn: +speed}, dataType: "jsonp"});
       status.html("Turning Right");
    };
 
    function left(){
       console.log('turn left');
-      $.ajax({url: testing+'/motion-control/update', data:{turn: -1}, dataType: "jsonp"});
+      $.ajax({url: testing+'/motion-control/update', data:{turn: -speed}, dataType: "jsonp"});
       status.html("Turning Left");
    };
 
@@ -38,13 +39,13 @@ $(function(){
 
    function st_l(){
       console.log('strafing right');
-      $.ajax({url: testing+'/motion-control/update', data:{strafe: +1}, dataType: "jsonp"});
+      $.ajax({url: testing+'/motion-control/update', data:{strafe: +speed}, dataType: "jsonp"});
       status.html('Strafing Right')
    };
 
    function st_r(){
       console.log('strafing left');
-      $.ajax({url: testing+'/motion-control/update', data:{strafe: -1}, dataType: "jsonp"});
+      $.ajax({url: testing+'/motion-control/update', data:{strafe: -speed}, dataType: "jsonp"});
       status.html('Strafing Left')
    };
 
@@ -80,59 +81,55 @@ $(function(){
 
    //Key Bindings
 
-   $(document).keydown(function(e) {
-      if(e.keyCode == 87){
+   $(document).keydown(function(e){
+      var key = e.keyCode;
+      switch (key)
+      {
+      case 87:
          console.log('w pressed');
          forward();
-      };
-   });
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 83){
+         break;
+      case 83:
          console.log('s pressed');
          backward();
-      }
-   });
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 68){
+         break;
+      case 68:
          console.log('d pressed');
          right();
-      };
-   });
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 65){
+         break;
+      case 65:
          console.log('a pressed');
          left();
-      };
-   })
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 32){
+         break;
+      case 32:
          console.log('space pressed');
          stop();
-      };
-   });
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 81){
+         break;
+      case 81:
          console.log('q pressed');
          st_l();
-      };
-   });
-
-   $(document).keydown(function(e){
-      if(e.keyCode == 69){
+         break;
+      case 69:
          console.log('e pressed');
          st_r();
+         break;
+      };  
+   });
+
+  $(document).keydown(function(e){
+      if(e.keyCode == 13){
+         console.log('half speed');
+         speed = .5;
+         console.log(speed);
       };
    });
 
-  $(document).keypress(function(e){
+  $(document).keyup(function(e){
       if(e.keyCode == 13){
-         console.log('enter held');
-      };
-   });
+         console.log('full speed');
+         speed = 1;
+         console.log(speed);
+      }
+  });
 
 });
